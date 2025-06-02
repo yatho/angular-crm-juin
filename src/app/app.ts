cd789c2fc7cc5fed356b34af8ad9b55a4eff83f4
Yann-thomas Le Moigne <yann.tho.le.moigne@gmail.com>
2025-06-02 17:36:01 +0200

    Initial

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class App {
   protected title = 'angularCRM';
+
+  protected product = {
+    name: 'Angular course',
+    price: 99.99,
+    description: 'Learn Angular from scratch with this comprehensive course.',
+    image: 'https://angular.io/assets/images/logos/angular/angular.png',
+    vendable: false
+  }
+
+  displayOk(event: any): void {
+    console.log('Event:', event, typeof event);
+    console.log('Ok button clicked');
+  }
+
+  priceOverFifty(price: number): boolean {
+    return price > 50;
+  }
+
+  protected tableProducts = [
+    this.product,
+    {
+      ...this.product,
+      price: 49.99,
+    }
+  ]
 }
